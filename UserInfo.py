aee9620c3c466277d69b30f8370d80d4f7928d89
Håkan Persson <hakan.persson@haninge.se>
2022-09-28 22:39:54 +0200

    Added PATCH method

--- UserInfo.py
+++ UserInfo.py
@@ -1,5 +1,6 @@
 import httpx
 import os
+import json
 # import traceback
 
 # from zeebe_worker import WorkerError
@@ -24,52 +25,60 @@ class UserInfo(object):
         return(pnum)
 
     async def worker(self, taskvars):
-        if taskvars['_HTTP_METHOD'] != "GET":
-            return {'_DIGIT_ERROR':"Only GET method is supported at the moment"}
-        if 'userid' not in taskvars:
+        if 'userid' not in taskvars or taskvars['userid'] == "":
             return {'_DIGIT_ERROR':"Missing mandatory variable 'userid'"}
-        if taskvars['userid'] == "":        # Not allowed!
-            return {'_DIGIT_ERROR':"Anonymous users are not allowed to request user info"}
-        userID = taskvars['personal_number'] if 'personal_number' in taskvars and taskvars['personal_number']!= "" else taskvars['userid']   # This might need to have restrictions?
+        userID = taskvars['personal_number'] if 'personal_number' in taskvars and taskvars['personal_number'] != "" else taskvars['userid']   # Is this still a valid request?
         userID = self.normpnum(userID)
 
-        args = {}
-        if '_NO_UPDATE' in taskvars:
-            args['_NO_UPDATE'] = ""
-        if '_NO_CASH' in taskvars:
-            args['_NO_CASH'] = ""
+        if taskvars['_HTTP_METHOD'] == "GET":
+            args = {}
+            if '_NO_UPDATE' in taskvars:
+                args['_NO_UPDATE'] = ""
+            if '_NO_CASH' in taskvars:
+                args['_NO_CASH'] = ""
 
-        async with httpx.AsyncClient(timeout=10, verify=False) as client:
-            r = await client.get(f"http://{USERINFOCASH}/userinfo/{userID}", params=args)
-            if r.status_code != 200:
-                return {'_DIGIT_ERROR': r.text, '_DIGIT_ERROR_STATUS_CODE': r.status_code}       # Error from userinfocash service
+            async with httpx.AsyncClient(timeout=10, verify=False) as client:
+                r = await client.get(f"http://{USERINFOCASH}/userinfo/{userID}", params=args)
+                if r.status_code != 200:
+                    return {'_DIGIT_ERROR': r.text, '_DIGIT_ERROR_STATUS_CODE': r.status_code}       # Error from userinfocash service
 
-        userinfo = r.json()
-        user = {}   # user values to return
+            userinfo = r.json()
+            user = {}   # user values to return
 
-        if userID.isdigit():        # Personnummer. External user!
-            user['personId'] = userinfo['PersonId'].strip()
-            if ',' in userinfo['GivenName']:
-                user['firstName'] = userinfo['GivenName'].split(',')[1].strip()    # Get first (given) name from last part of 'GivenName'
-            else:
-                user['firstName'] = userinfo['GivenName'].strip()      # Just grab what is there
-            user['lastName'] = userinfo['LastName'].strip()
-            user['fullName'] = userinfo['FirstName'].strip()+" "+userinfo['LastName'].strip()
-            user['address'] = userinfo['Address'].strip()
-            user['zipcode'] = userinfo['ZipCode'].strip()
-            user['city'] = userinfo['City'].strip()
-            # user['country'] = userinfo['Country'].strip()       # Skip this for now
-            user['municipalityCode'] = userinfo['MunicipalityCode'].strip()
-            for k,v in userinfo.items():
-                if k not in ['PersonId','Address','BirthPlace','City','CivilStatus','Country','FirstName','GivenName','LastName','ZipCode','MunicipalityCode','Parish','Relation']: # List of KIR data
-                    user[k] = v     # Added extra data that are not from KIR
-        else:                   # Internal user
-            # user = userinfo
-            user['firstName'] = userinfo['givenName']
-            user['lastName'] = userinfo['surname']
-            user['fullName'] = userinfo['displayName']
-            user['department'] = userinfo['department']
-            user['email'] = userinfo['mail']
+            if userID.isdigit():        # Personnummer. External user!
+                user['personId'] = userinfo['PersonId'].strip()
+                if ',' in userinfo['GivenName']:
+                    user['firstName'] = userinfo['GivenName'].split(',')[1].strip()    # Get first (given) name from last part of 'GivenName'
+                else:
+                    user['firstName'] = userinfo['GivenName'].strip()      # Just grab what is there
+                user['lastName'] = userinfo['LastName'].strip()
+                user['fullName'] = userinfo['FirstName'].strip()+" "+userinfo['LastName'].strip()
+                user['address'] = userinfo['Address'].strip()
+                user['zipcode'] = userinfo['ZipCode'].strip()
+                user['city'] = userinfo['City'].strip()
+                # user['country'] = userinfo['Country'].strip()       # Skip this for now
+                user['municipalityCode'] = userinfo['MunicipalityCode'].strip()
+                for k,v in userinfo.items():
+                    if k not in ['PersonId','Address','BirthPlace','City','CivilStatus','Country','FirstName','GivenName','LastName','ZipCode','MunicipalityCode','Parish','Relation']: # List of KIR data
+                        user[k] = v     # Added extra data that are not from KIR
+            else:                   # Internal user
+                # user = userinfo
+                user['firstName'] = userinfo['givenName']
+                user['lastName'] = userinfo['surname']
+                user['fullName'] = userinfo['displayName']
+                user['department'] = userinfo['department']
+                user['email'] = userinfo['mail']
 
-        return {'user': user}     # Return what we found
-        # return user     # Return what we found
+            return {'user': user}     # Return what we found
+
+        elif taskvars['_HTTP_METHOD'] == "PATCH":
+            if '_JSON_BODY' not in taskvars:
+                return {'_DIGIT_ERROR':"Missing JSON-body in request (nothing to patch)!"}
+            patch_data = json.loads(taskvars['_JSON_BODY'])
+            async with httpx.AsyncClient(timeout=10, verify=False) as client:
+                r = await client.patch(f"http://{USERINFOCASH}/userinfo/{userID}", json=patch_data)
+                if r.status_code != 200:
+                    return {'_DIGIT_ERROR': r.text, '_DIGIT_ERROR_STATUS_CODE': r.status_code}       # Error from userinfocash service
+        else:
+            return {'_DIGIT_ERROR':"Only GET and PATCH methods are supported."}
+
--- http_server.py
+++ http_server.py
@@ -1,6 +1,5 @@
-import asyncio
 import os
-import time
+import json
 import logging
 
 from aiohttp import web, ClientSession, BasicAuth, ClientTimeout, log
@@ -23,6 +22,8 @@ async def http_handler(request):
 
     query_args = dict(request.query)        # Grab all query args
     query_args['_HTTP_METHOD'] = request.method  # Add method
+    if request.can_read_body and request.content_type == 'application/json':
+        query_args['_JSON_BODY'] = json.dumps(await request.json())
 
     worker = request.app['WORKER']      # Get worker refeence
     resp = await worker(query_args)     # Run the worker
@@ -56,52 +57,3 @@ async def http_server(worker, port):
     await site.start()
 
     return runner
-
-
-
-""" 
-This is just for testing
-"""
-# if __name__ == "__main__":
-#     from UserInfo import UserInfo
-#     # Enable logging. INFO is default. DEBUG if requested
-#     logging.basicConfig(level=logging.DEBUG if DEBUG_MODE else logging.INFO, format=LOGFORMAT)
-
-#     worker = UserInfo()           # Create an instance of the worker
-
-#     # Our web application instance
-#     app = web.Application()
-#     app['WORKER'] = worker.worker
-
-#     # Register handlers
-#     routes = [
-#         web.get(f"/{worker.queue_name}", http_handler),
-#         web.put(f"/{worker.queue_name}", http_handler),
-#         web.post(f"/{worker.queue_name}", http_handler),
-#         web.patch(f"/{worker.queue_name}", http_handler),
-#         web.delete(f"/{worker.queue_name}", http_handler)
-#     ]
-#     app.add_routes(routes)
-
-#     # Start the web server!
-#     if not DEBUG_MODE:
-#         web.run_app(app, access_log=None, print=None)       # Don't be verbose
-#     else:
-#         web.run_app(app, port=8081)    # Run with defaults
-
-
-
-
-    # loop = asyncio.new_event_loop()
-    # http_runner = loop.create_task(http_server(worker))
-
-    # try:
-    #     asyncio.set_event_loop(loop)
-    #     loop.run_forever()
-    # except (KeyboardInterrupt, GracefulExit):  # pragma: no cover
-    #     pass
-    # finally:
-    #     # _cancel_tasks({main_task}, loop)
-    #     # _cancel_tasks(all_tasks(loop), loop)
-    #     loop.run_until_complete(loop.shutdown_asyncgens())
-    #     loop.close()
